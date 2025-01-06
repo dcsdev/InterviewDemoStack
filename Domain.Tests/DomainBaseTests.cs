c53f2c97b97dadc0132d027c08a19f0aceaef3a4
Douglas Spencer <douglasc.spencer+1@gmail.com>
2025-01-06 18:41:34 -0500

    Add Test Project

---
+++ Domain.Tests/DomainBaseTests.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Domain;
+using Xunit;
+
+namespace Domain.Tests
+{
+    public class DomainBaseTests
+    {
+        [Theory]
+        [InlineData(0,0)]
+        [InlineData(2, 0)]
+        [InlineData(-1, 0)]
+        public void shouldInstantiateEmptyActivityList(int listSize, int expectedListSize )
+        {
+            //Arrange
+            DomainBase domain = new DomainBase();
+
+            //Assert
+            Assert.Equal(listSize, domain.Activities.Count);
+        }
+
+        //[Fact]
+        //public void shouldInstantiatOriginalGraphResourceLocatorBlankString()
+        //{
+        //    //Arrange
+        //    DomainBase domain = new DomainBase();
+
+        //    //Assert
+        //    Assert.Equal(string.Empty, domain.OriginalGraphResourceLocator);
+        //}
+
+        //[Fact]
+        //public void shouldInstantiatFinalGraphResourceLocatorBlankString()
+        //{
+        //    //Arrange
+        //    DomainBase domain = new DomainBase();
+
+        //    //Assert
+        //    Assert.Equal(string.Empty, domain.FinalGraphResourceLocator);
+        //}
+    }
+}
+
+
+//public string OriginalGraphResourceLocator { get; set; } = string.Empty;
+//public string FinalGraphResourceLocator { get; set; } = String.Empty;
+//public List<Activity> Activities { get; set; } = new List<Activity>();
